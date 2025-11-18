ca17fd7cc3df55c11910e717a798b3cefe239b03
RODea_L00203120 <L00203120@atu.ie>
2025-11-18 22:23:47 +0000

    Migrate to HCP Terraform backend for Phase 2

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,20 @@
 terraform {
-  backend "s3" {
-    bucket         = "terraform-s3-l00203120-rod"
-    key            = "infrastructure/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"
+  cloud {
+    organization = "iac-lab-rodea"
+    
+    workspaces {
+      name = "IaC_Lab_2_Terraform"
+    }
   }
 }
+
+
+#terraform {
+# backend "s3" {
+#    bucket         = "terraform-s3-l00203120-rod"
+#    key            = "infrastructure/terraform.tfstate"
+#    region         = "us-east-1"
+#   encrypt        = true
+#   dynamodb_table = "terraform-state-lock"
+# }
+# }
